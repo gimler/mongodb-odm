cf013d9533a757ba2119ca39e4994c5bc144f239
Gordon Franke <info@nevalon.de>
2010-05-21 08:19:19 +0200

    remove duplicate code

--- lib/Doctrine/ODM/MongoDB/Query.php
+++ lib/Doctrine/ODM/MongoDB/Query.php
@@ -497,7 +497,7 @@ class Query
     public function sort($fieldName, $order)
     {
         $this->_sort = array();
-        $this->_sort[$fieldName] = strtolower($order) === 'asc' ? 1 : -1;
+        $this->addSort($fieldName, $order);
         return $this;
     }
 
